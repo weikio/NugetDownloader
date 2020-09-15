f25eca3c837edd6d512356666af36e06d20d39db
Mikko Uuksulainen <mikko.uuksulainen@adafy.com>
2020-09-15 14:07:57 +0300

    Move NugetDownloader to the own solution from PluginFramework

---
+++ src/Weikio.NugetDownloader/ConsoleLogger.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading.Tasks;
+using NuGet.Common;
+
+namespace Weikio.NugetDownloader
+{
+    public class ConsoleLogger : LoggerBase
+    {
+        public override void Log(ILogMessage message)
+        {
+            switch (message.Level)
+            {
+                case LogLevel.Debug:
+                    Console.WriteLine($"DEBUG - {message}");
+                    break;
+
+                case LogLevel.Verbose:
+                    Console.WriteLine($"VERBOSE - {message}");
+                    break;
+
+                case LogLevel.Information:
+                    Console.WriteLine($"INFORMATION - {message}");
+                    break;
+
+                case LogLevel.Minimal:
+                    Console.WriteLine($"MINIMAL - {message}");
+                    break;
+
+                case LogLevel.Warning:
+                    Console.WriteLine($"WARNING - {message}");
+                    break;
+
+                case LogLevel.Error:
+                    Console.WriteLine($"ERROR - {message}");
+                    break;
+            }
+        }
+
+        public override Task LogAsync(ILogMessage message)
+        {
+            Log(message);
+
+            return Task.CompletedTask;
+        }
+    }
+}
