21685c4031d15c82d1433dabb9a4d7cd4bd5de32
Mikko Uuksulainen <mikko.uuksulainen@adafy.com>
2020-09-16 10:49:31 +0300

    Add test project

---
+++ tests/integration/Weikio.NugetDownloader.Tests/NugetDownloaderTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Weikio.NugetDownloader.Tests
+{
+    public class NugetDownloaderTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
